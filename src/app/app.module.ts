16874d7d3c46dd034ba613d95b6b7a55332b4147
Anis <anis.kraiem1996@gmail.com>
2021-11-30 02:23:44 +0100

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FactureComponent} from "./facture/facture/facture.component";
+import {FournisseurComponent} from "./fournisseur/fournisseur/fournisseur.component";
+import {ProduitComponent} from "./produit/produit/produit.component";
+import {RayonComponent} from "./produit/rayon/rayon.component";
+import {StockComponent} from "./produit/stock/stock.component";
+import {ClientComponent} from "./client/client/client.component";
+import {LoginComponent} from "./client/login/login.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    ClientComponent,
+    FactureComponent,
+    FournisseurComponent,
+    ProduitComponent,
+    RayonComponent,
+    StockComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client/client.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    ClientComponent
+    ClientComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
